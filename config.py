45336b9ec2d270ea1b4435a028501d77e5c943db
alexei37d-bit <alexei37d@gmail.com>
2026-06-30 21:17:20 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+# Бот сначала ищет переменные от хостинга, а если их нет — берет данные из кавычек
+BOT_TOKEN = os.getenv("BOT_TOKEN") or "7548847926:AAHszy_asqXAOX6faKs6Z32FJLQYc1DOOdY"
+ADMIN_ID = int(os.getenv("ADMIN_ID") or 7921743592)
 
-# Директория для временных файлов
 DOWNLOAD_DIR = "downloads"
+
 if not os.path.exists(DOWNLOAD_DIR):
     os.makedirs(DOWNLOAD_DIR)
